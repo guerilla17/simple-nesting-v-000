295ef147941b55694a65af5f4ca953d4fdc2e3e3
Gigi M <mmgamao1@gmail.com>
2019-03-14 20:25:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,6 +104,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
+   end
+    programmer_hash[:alan_kay][:known_for]=> "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
